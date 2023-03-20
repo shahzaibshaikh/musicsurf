15ff27459526404a5abadbbd16e851b1e634139b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-21 00:26:23 +0500

    🚀 setup up initial vite project and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }}
+    >
+      <GridItem area='nav' bg='red'>
+        Nav
+      </GridItem>
+      <GridItem area='aside' bg='blue'>
+        Aside
+      </GridItem>
+
+      <GridItem area='main' bg='yellow'>
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
