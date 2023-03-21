6c997cd69cfb806740c9269e1523bdc232de95d9
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-22 02:44:19 +0500

    📦 NEW: setup redux store

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import theme from './styles/theme';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/store/albumSlice.ts
@@ -0,0 +1,6 @@
+import { createSlice } from '@reduxjs/toolkit';
+export const albumSlice = createSlice({
+  name: 'theme',
+  initialState: [],
+  reducers: {}
+});
