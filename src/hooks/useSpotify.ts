f794cd57215d958acbed11be36591bf65aa0c1d0
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-28 01:55:00 +0500

    🚀 fixed authorization flow

--- src/hooks/useSpotify.ts
+++ src/hooks/useSpotify.ts
@@ -1,36 +1,29 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setToken } from '../store/slices/spotifySlice';
+import { setError, setLoading, setToken } from '../store/slices/spotifySlice';
 
 function useSpotify() {
   const dispatch = useDispatch<any>();
   const { loading, error, token } = useSelector((state: any) => state.spotify);
 
   useEffect(() => {
-    if (!token) {
-      // check if token exists
-      const hashParams = window.location.hash
-        .substring(1)
-        .split('&')
-        .reduce(function (result: any, item: string) {
-          var parts = item.split('=');
-          result[parts[0]] = parts[1];
-          return result;
-        }, {});
+    const fetchData = async () => {
+      try {
+        dispatch(setLoading(true));
 
-      if (hashParams.access_token) {
-        dispatch(setToken(hashParams.access_token));
-      } else {
-        const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-        const REDIRECT_URI: any = import.meta.env.VITE_REDIRECT_URI;
-        const SCOPE = 'user-read-private%20user-read-email';
-        const STATE = 'spotify-auth';
+        const response = await axios.get(import.meta.env.VITE_AUTH_BASE_URL);
 
-        const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&state=${STATE}`;
+        dispatch(setToken(response.data.access_token));
 
-        window.location.href = url;
+        dispatch(setLoading(false));
+      } catch (error: any) {
+        dispatch(setError(error));
+      } finally {
+        dispatch(setLoading(false));
       }
-    }
+    };
+    if (!token) fetchData();
   }, [dispatch, token]);
 
   return { loading, error, token };
