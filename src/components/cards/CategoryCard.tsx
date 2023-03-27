29db7ebad727b8e4e2b557aca6dd0f30c5cc7d2d
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-28 04:45:19 +0500

    👌 added linking on category card

--- src/components/cards/CategoryCard.tsx
+++ src/components/cards/CategoryCard.tsx
@@ -1,4 +1,5 @@
-import { Box, Text, Image } from '@chakra-ui/react';
+import { Box, Text, Image, LinkBox, LinkOverlay } from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
 import { Categories } from '../../store/slices/categorySlice';
 
 interface CategoryCardProps {
@@ -7,41 +8,46 @@ interface CategoryCardProps {
 
 function CategoryCard({ data }: CategoryCardProps): JSX.Element {
   return (
-    <Box
-      borderRadius={8}
-      height='210px'
-      overflow='hidden'
-      position='relative'
-      boxShadow='0 8px 8px rgba(0, 0, 0, .2)'
-    >
-      <Image src={data.icons[0].url} objectFit='cover' width='100%' height='100%' />
+    <LinkBox>
       <Box
-        position='absolute'
-        top='0'
-        left='0'
-        width='100%'
-        height='100%'
-        zIndex='1'
-        bgGradient='linear(0deg, rgba(0,0,0,0.9990589985994398) 4%, rgba(0,0,0,0) 64%)'
-        _hover={{
-          transition: '500ms',
-          backdropFilter: 'blur(6px)'
-        }}
-      />
-      <Text
-        position='absolute'
-        top='80%'
-        left='50%'
-        transform='translate(-50%, -50%)'
-        zIndex='2'
-        color='white'
-        fontWeight={700}
-        fontSize='2xl'
-        textAlign='center'
+        borderRadius={8}
+        height='210px'
+        overflow='hidden'
+        position='relative'
+        boxShadow='0 8px 8px rgba(0, 0, 0, .2)'
       >
-        {data.name}
-      </Text>
-    </Box>
+        <Image src={data.icons[0].url} objectFit='cover' width='100%' height='100%' />
+        <LinkOverlay as={ReactLink} to={`/categories/${data.id}`}>
+          <Box
+            position='absolute'
+            top='0'
+            left='0'
+            width='100%'
+            height='100%'
+            zIndex='1'
+            bgGradient='linear(0deg, rgba(0,0,0,0.9990589985994398) 4%, rgba(0,0,0,0) 64%)'
+            _hover={{
+              transition: '500ms',
+              backdropFilter: 'blur(6px)'
+            }}
+          />
+
+          <Text
+            position='absolute'
+            top='80%'
+            left='50%'
+            transform='translate(-50%, -50%)'
+            zIndex='2'
+            color='white'
+            fontWeight={700}
+            fontSize='2xl'
+            textAlign='center'
+          >
+            {data.name}
+          </Text>
+        </LinkOverlay>
+      </Box>
+    </LinkBox>
   );
 }
 
