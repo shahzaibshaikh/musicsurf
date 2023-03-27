b51d0e74d4b6f9fdba7afe1c9bd3804fa225d36c
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-27 18:26:32 +0500

    👌 made component placeholders for playlist

---
+++ src/components/layout/PlaylistDetailHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PlaylistDetailHeader() {
+  return <div></div>;
+}
+
+export default PlaylistDetailHeader;
--- src/pages/PlaylistDetailScreen.tsx
+++ src/pages/PlaylistDetailScreen.tsx
@@ -1,7 +1,26 @@
-import { Box } from '@chakra-ui/react';
+import TrackListing from '../components/layout/TrackListing';
+import { Box, Divider } from '@chakra-ui/react';
+import { useState } from 'react';
+import { average } from 'color.js';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function PlaylistDetailScreen() {
-  return <Box className='grid-container'>Playlist Detail Screen</Box>;
+  const { playlistID } = useParams();
+  const { token } = useSelector((state: any) => state.spotify);
+  let colorGenerator: string;
+  const [color, setColor] = useState('');
+  let count: number = 0;
+
+  return (
+    <Box
+      className='playlist-grid-container'
+      background={`linear-gradient(180deg, ${color} 0%, rgba(18, 18, 18, 1) 100%)`}
+    >
+      PlayList Detail Screen
+      <Divider color='#121212' marginTop='80px' marginBottom='40px' />
+    </Box>
+  );
 }
 
 export default PlaylistDetailScreen;
