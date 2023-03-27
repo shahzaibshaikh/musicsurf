c9f0c8d5cbeffe31bd4431e97e5b77dbb5f18495
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-27 18:08:05 +0500

    👌 added playlist linking on feature tracks

--- src/components/cards/FeaturedPlaylistCard.tsx
+++ src/components/cards/FeaturedPlaylistCard.tsx
@@ -1,4 +1,13 @@
-import { Box, Image, HStack, Text, CardBody, Card, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Image,
+  HStack,
+  Card,
+  Heading,
+  LinkBox,
+  LinkOverlay
+} from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
 import { PlaylistItems } from '../../store/slices/searchSlice';
 
 interface FeaturedPlaylistCardProps {
@@ -7,30 +16,34 @@ interface FeaturedPlaylistCardProps {
 
 function FeaturedPlaylistCard({ data }: FeaturedPlaylistCardProps) {
   return (
-    <Card
-      borderRadius={4}
-      background='rgb(90,90,90, 0.2)'
-      overflow='hidden'
-      _hover={{ background: 'rgb(90,90,90, 0.4)', transition: '300ms' }}
-      boxShadow='0 8px 8px rgba(0, 0, 0, .1)'
-    >
-      <HStack alignItems='center'>
-        <Image
-          boxShadow='0 8px 14px rgba(0, 0, 0, .6)'
-          boxSize='80px'
-          src={data?.images[0]?.url}
-        />
-        <Heading
-          size='1xl'
-          padding={3}
-          noOfLines={1}
-          overflowWrap='break-word'
-          color='white'
-        >
-          {data?.name}
-        </Heading>
-      </HStack>
-    </Card>
+    <LinkBox>
+      <Card
+        borderRadius={4}
+        background='rgb(90,90,90, 0.2)'
+        overflow='hidden'
+        _hover={{ background: 'rgb(90,90,90, 0.4)', transition: '300ms' }}
+        boxShadow='0 8px 8px rgba(0, 0, 0, .1)'
+      >
+        <HStack alignItems='center'>
+          <Image
+            boxShadow='0 8px 14px rgba(0, 0, 0, .6)'
+            boxSize='80px'
+            src={data?.images[0]?.url}
+          />
+          <LinkOverlay as={ReactLink} to={`/playlist/${data?.id}`}>
+            <Heading
+              size='1xl'
+              padding={3}
+              noOfLines={1}
+              overflowWrap='break-word'
+              color='white'
+            >
+              {data?.name}
+            </Heading>
+          </LinkOverlay>
+        </HStack>
+      </Card>
+    </LinkBox>
   );
 }
 
