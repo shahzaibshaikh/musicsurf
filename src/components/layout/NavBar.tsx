bebd62a287b1a7f7448c13e884a95ee3c35ba0ba
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-29 03:57:13 +0500

    ✅ finalized search bar conditional rendering

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,16 +1,21 @@
 import { HStack, Image, Show } from '@chakra-ui/react';
+import { useLocation } from 'react-router-dom';
 import LogoIcon from '../../assets/favicon.svg';
 import SearchInput from '../common/SearchInput';
 
 function NavBar(): JSX.Element {
+  const location = useLocation();
+
+  const shouldRenderSearchInput =
+    !location.pathname.split('/')[2] && location.pathname.split('/')[1] === 'search';
+
   return (
     <HStack className='nav-styles' spacing={5}>
       <Show breakpoint='(max-width: 992px)'>
         <Image src={LogoIcon} height='35px' />
       </Show>
 
-      {!window.location.pathname.split('/')[2] &&
-        window.location.pathname.split('/')[1] === 'search' && <SearchInput />}
+      {shouldRenderSearchInput && <SearchInput />}
     </HStack>
   );
 }
