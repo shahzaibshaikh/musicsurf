c73a4f96af056db9ce903266b2c32a90b3b10269
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-29 03:53:38 +0500

    👌 added extra condition on navbar component

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -3,18 +3,14 @@ import LogoIcon from '../../assets/favicon.svg';
 import SearchInput from '../common/SearchInput';
 
 function NavBar(): JSX.Element {
-  console.log(
-    !window.location.pathname.split('/')[2] &&
-      window.location.pathname.split('/')[1] === 'search'
-  );
-
   return (
     <HStack className='nav-styles' spacing={5}>
       <Show breakpoint='(max-width: 992px)'>
         <Image src={LogoIcon} height='35px' />
       </Show>
 
-      {window.location.pathname === '/search' && <SearchInput />}
+      {!window.location.pathname.split('/')[2] &&
+        window.location.pathname.split('/')[1] === 'search' && <SearchInput />}
     </HStack>
   );
 }
