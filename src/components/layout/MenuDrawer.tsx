7520e9ed0c5149c59be6477d715aeebeedf6e4aa
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-29 06:23:26 +0500

    🚀 added fully functional side drawer

--- src/components/layout/MenuDrawer.tsx
+++ src/components/layout/MenuDrawer.tsx
@@ -5,9 +5,32 @@ import {
   DrawerCloseButton,
   DrawerContent,
   DrawerOverlay,
-  IconButton
+  HStack,
+  Icon,
+  IconButton,
+  Link,
+  List,
+  Text,
+  ListItem
 } from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
 import { IoIosArrowBack } from 'react-icons/io';
+import { SiGooglehome } from 'react-icons/si';
+import { TbCategory } from 'react-icons/tb';
+
+const menuOptions = [
+  { id: 1, name: 'Home', slug: 'home', path: '/', icon: SiGooglehome },
+  { id: 2, name: 'Search', slug: 'search', path: '/search', icon: FaSearch },
+  {
+    id: 3,
+    name: 'Categories',
+    slug: 'categories',
+    path: '/categories',
+    icon: TbCategory
+  }
+  // { id: 4, name: 'Your Library', slug: 'library', path: '/library', icon: IoLibrary }
+];
 
 interface MenuDrawerProps {
   isOpen: boolean;
@@ -16,9 +39,9 @@ interface MenuDrawerProps {
 
 function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
   return (
-    <Drawer isOpen={isOpen} placement='right' onClose={onClose}>
+    <Drawer isOpen={isOpen} placement='right' onClose={onClose} size='xs'>
       <DrawerOverlay />
-      <DrawerContent background='rgba(0, 0, 0, 0.2)' backdropFilter='blur(8px)'>
+      <DrawerContent background='rgba(0, 0, 0, 0.4)' backdropFilter='blur(8px)'>
         <DrawerCloseButton
           marginRight={4}
           marginTop={2}
@@ -34,7 +57,25 @@ function MenuDrawer({ isOpen, onClose }: MenuDrawerProps) {
         />
 
         <DrawerBody>
-          <Box>Hello.</Box>
+          <Box className='menu-drawer-container'>
+            <List paddingLeft={3} spacing={5} marginTop={20}>
+              {menuOptions.map(option => (
+                <ListItem key={option.id}>
+                  <Link
+                    onClick={onClose}
+                    as={ReactLink}
+                    to={option.path}
+                    _hover={{ textDecoration: 'none' }}
+                  >
+                    <HStack spacing={5} color='white'>
+                      <Icon as={option.icon} boxSize={5} />
+                      <Text>{option.name}</Text>
+                    </HStack>
+                  </Link>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
