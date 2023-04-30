39217af2d4a718f2a38e6f78da1689369f639543
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-30 18:40:41 +0500

    👌 IMPROVE: reverted heading font size

--- src/pages/HomeGrid.tsx
+++ src/pages/HomeGrid.tsx
@@ -28,7 +28,7 @@ function HomeGrid(): JSX.Element {
           )}
       </SimpleGrid>
 
-      <Heading fontSize='xl' marginBottom={4} color='white'>
+      <Heading fontSize='2xl' marginBottom={4} color='white'>
         New Releases
       </Heading>
 
